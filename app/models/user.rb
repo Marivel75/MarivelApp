e0d91a2bcff7969f08da5283b2d620b69380bcbb
Mikaël <rocavel.dev@zoho.eu>
2020-03-25 22:10:38 +0100

    tout fonctionnel, encore qlq views à améliorer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :created_trips, class_name: "Trip", foreign_key: "id"
+  has_many :created_trips, foreign_key: "author_id", class_name: "Trip"
 
   after_create :welcome_send
 
