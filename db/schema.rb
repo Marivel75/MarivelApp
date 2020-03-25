ea060131901bb0aa9f5b009cd35075ea64a37df9
AdrienCastelbou <acastelbou1@gmail.com>
2020-03-25 11:54:06 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_202007) do
+ActiveRecord::Schema.define(version: 2020_03_25_092339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,8 @@ ActiveRecord::Schema.define(version: 2020_03_22_202007) do
     t.string "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_trips_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -105,4 +107,5 @@ ActiveRecord::Schema.define(version: 2020_03_22_202007) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "trips", "users", column: "author_id"
 end
