ae87ef7bb121563c9a9627e5140853aed6887383
Mikaël <rocavel.dev@zoho.eu>
2020-03-27 11:14:02 +0100

    ajustements alertes et before_action dans les controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -86,6 +86,13 @@ class TripsController < ApplicationController
     end
 
     def is_current_user?
-        redirect_to new_user_session_path if !current_user
+      set_trip
+       if !current_user
+        flash.alert = "Veuillez vous connecter"
+        redirect_to new_user_session_path
+        elsif  @trip.author != current_user
+          flash.alert = "Vous n'avez pas les droits nécessaires"
+          redirect_to @trip
+       end
     end
 end
