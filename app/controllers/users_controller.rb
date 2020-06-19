a2f9f45708a44cf1dd94e142784a060cf62ff7a9
Mikaël <rocavel.dev@zoho.eu>
2020-06-19 14:53:39 +0200

    amélioration des views partner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   before_action :set_user
   before_action :set_current_user
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user.id), notice: "Les modifications ont été enregistrées."
   end
 
   private
@@ -14,5 +23,9 @@ class UsersController < ApplicationController
     if !user_signed_in? || @user.id != current_user.id && current_user.role != 'admin'
       redirect_to root_path, alert:"Accès non autorisé"
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :description)
   end
 end
